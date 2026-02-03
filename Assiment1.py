73cf3707c5ce1966fbaaefa804e4fff0b01301db
dkskr2134-hash <dkskr2134@gmail.com>
2026-02-03 08:01:30 +0530

    Add files via upload

---
+++ Assiment1.py
@@ -0,0 +1,26 @@
+##Task 1: Perform Basic Mathematical Operations
+
+a=float(input("Enter the first number: "))
+b=float(input("Enter the second number: "))
+
+"Addition"
+A=a+b
+"Subtraction"
+B=a-b
+"Multiplication"
+C=a*b
+"Division"
+D=a/b
+
+print("Division is ",D)
+print("Multiplication is ",C)
+print("Addition is ",A)
+print("Subtraction is ",B)
+
+##Task 2: Create a Personalized Greeting
+
+name=input("Enter your first name: ")
+surname=input("Enter your last name: ")
+
+print("Hello " , name," ", surname,"!", " Welcome to the Python program.",sep="")
+
