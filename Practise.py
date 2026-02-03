91d84b09d14313da0a04e290089fe4ff1b39b49a
dkskr2134-hash <dkskr2134@gmail.com>
2026-02-03 07:57:26 +0530

    Add files via upload

---
+++ Practise.py
@@ -0,0 +1,30 @@
+#Area of triangle
+""""
+area of a triangle
+s=(a+b+c)/2
+area=(s*(s-a)*(s-b)*(s-c)) ** 0.5(square root)
+"""
+"""
+a=float(input("Enter first number:"))
+b=float(input("Enter second number:"))
+c=float(input("Enter third number:"))
+s=(a+b+c)/2
+area=(s*(s-a)*(s-b)**2)**1/2
+print("The area of the triangle is ",round(area,3))
+"""
+#simple Intrest
+"""
+Simple interest = (P * R * T) / 100
+P = Principal amount
+R = rate of interest
+T = time duration
+"""
+p=float(input("Enter principle number:"))
+r=float(input("Enter rate of interest:"))
+t=float(input("Enter duration:"))
+I=(p*r*t)/100
+
+print("The interest rate is ",round(I,3))
+
+
+
